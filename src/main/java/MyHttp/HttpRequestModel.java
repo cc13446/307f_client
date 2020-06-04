fbab6bd431a8f5dc367aeac1743f2961f3c73280
cc13446 <1344613920@qq.com>
2020-06-04 19:54:06 +0800

    cc 2020/06/04 19:53

--- src/main/java/MyHttp/HttpRequestModel.java
+++ src/main/java/MyHttp/HttpRequestModel.java
@@ -11,52 +11,38 @@ import java.time.Duration;
 import java.util.concurrent.Executors;
 
 public class HttpRequestModel {
+    public String url;
+    public String requestMethod;
+
+    public HttpRequestModel(String url, String requestMethod) {
+        this.url = url;
+        this.requestMethod = requestMethod;
+    }
+
     public JSONObject send(JSONObject json) throws IOException, InterruptedException {
         //创建 builder
         HttpClient.Builder builder = HttpClient.newBuilder();
-
-        //链式调用
-        HttpClient client = builder
-                //http 协议版本  1.1 或者 2
-                .version(HttpClient.Version.HTTP_2) //.version(HttpClient.Version.HTTP_1_1)
-
-                //连接超时时间，单位为毫秒
-                .connectTimeout(Duration.ofMillis(5000)) //.connectTimeout(Duration.ofMinutes(1))
-
-                //连接完成之后的转发策略
-                .followRedirects(HttpClient.Redirect.NEVER) //.followRedirects(HttpClient.Redirect.ALWAYS)
-
-                //指定线程池
+        HttpClient client = builder.version(HttpClient.Version.HTTP_2)
+                .connectTimeout(Duration.ofMillis(5000))
+                .followRedirects(HttpClient.Redirect.NEVER)
                 .executor(Executors.newFixedThreadPool(5))
-
-                //创建完成
                 .build();
         //创建 builder
         HttpRequest.Builder reBuilder = HttpRequest.newBuilder();
-
-        //链式调用
-        HttpRequest request = reBuilder
-
-                //存入消息头
-                //消息头是保存在一张 TreeMap 里的
-                .header("Content-Type", "application/json")
-
-                //http 协议版本
-                .version(HttpClient.Version.HTTP_1_1)
-
-                //url 地址
-                .uri(URI.create("http://localhost:8080"))
-
-                //超时时间
-                .timeout(Duration.ofMillis(5009))
-
-                .method("get", HttpRequest.BodyPublishers.ofString(json.toString()))
-
-                //创建完成
+        HttpRequest request = reBuilder.header("Content-Type", "application/json")
+                .version(HttpClient.Version.HTTP_2)
+                .uri(URI.create("http://localhost" + url))
+                .timeout(Duration.ofMillis(5000))
+                .method(requestMethod, HttpRequest.BodyPublishers.ofString(json.toString()))
                 .build();
-
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JSONObject temp = JSONObject.fromObject(response.body());
-        return temp;
+        if(response.statusCode() == 200)
+        {
+            return JSONObject.fromObject(response.body());
+        }
+        else
+        {
+            return null;
+        }
     }
 }
--- src/main/java/MyListener/GuiListener.java
+++ src/main/java/MyListener/GuiListener.java
@@ -10,23 +10,20 @@ import java.awt.event.ActionListener;
 
 public class GuiListener implements ActionListener {
     private JFrame frame;
-    private HttpRequestModel httpRequestModel;
     private Room room;
     public GuiListener(JFrame frame, Room room){
         super();
         this.room = room;
         this.frame = frame;
-        httpRequestModel = new HttpRequestModel();
     }
     public void actionPerformed(ActionEvent e) {
-        // 利用getActionCommand获得按钮名称
-        // 也可以利用getSource()来实现
-        // if (e.getSource() ==button1)
         try {
             String buttonName = e.getActionCommand();
+            JSONObject json = new JSONObject();
+            HttpRequestModel httpRequestModel = null;
             if (buttonName.equals("开关")) {
-                JSONObject json = new JSONObject();
                 if(room.getState() == State.OFF){
+                    httpRequestModel = new HttpRequestModel("/room/serve", "POST");
                     room.setState(State.ON);
                     json.put("scheduleType", ScheduleType.NEW_REQUEST);
                     json.put("roomId", room.getRoomId());
@@ -37,31 +34,39 @@ public class GuiListener implements ActionListener {
                     json.put("fanSpeed", room.getFanSpeed());
                 }
                 else if(room.getState() == State.ON){
+                    httpRequestModel = new HttpRequestModel("/room/serve", "PUT");
                     room.setState(State.OFF);
                     json.put("scheduleType", ScheduleType.CLOSE);
                     json.put("state", room.getState());
                 }
-                System.out.println(httpRequestModel.send(json));
+
             }
             else if (buttonName.equals("温度") && room.getState() == State.ON){
-                JSONObject json = new JSONObject();
+                httpRequestModel = new HttpRequestModel("/room/temp", "POST");
                 room.setTargetTemp(26.0);
                 json.put("scheduleType", ScheduleType.CHANGE_TEMP);
                 json.put("targetTemp", room.getTargetTemp());
-                System.out.println(httpRequestModel.send(json));
             }
             else if (buttonName.equals("风速") && room.getState() == State.ON){
-                JSONObject json = new JSONObject();
                 room.setFanSpeed(FanSpeed.LOW);
                 json.put("scheduleType", ScheduleType.CHANGE_FAN_SPEED);
                 json.put("fanSpeed", room.getFanSpeed());
-                System.out.println(httpRequestModel.send(json));
+                httpRequestModel = new HttpRequestModel("/room/fan", "POST");
             }
             else if (buttonName.equals("模式") && room.getState() == State.ON){
                 JOptionPane.showMessageDialog(frame,"你要相信：模式改变了" );
+                httpRequestModel = new HttpRequestModel("/room/mode", "POST");
             }
             else{
                 JOptionPane.showMessageDialog(frame,"先开机哦" );
+                httpRequestModel = new HttpRequestModel("/room/mode", "POST");
+            }
+            JSONObject temp = httpRequestModel.send(json);
+            if(temp != null){
+                System.out.println(temp);
+            }
+            else{
+                System.out.println("发送失败");
             }
         } catch (Exception ex) {
             ex.printStackTrace();
